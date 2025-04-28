884fbc5604738f2d2aa47a04ff30d3266e03a588
Alan <148771102+Alan2217@users.noreply.github.com>
2025-04-28 16:27:40 +1000

    Add files via upload

---
+++ Moter light/Buttom.h
@@ -0,0 +1,6 @@
+#ifndef __BUTTOM_H
+#define __BUTTOM_H
+
+void Buttom_Init(void);
+uint8_t Buttom_GetNumber(void);
+#endif 
---
+++ Moter light/Lumasensor.h
@@ -0,0 +1,6 @@
+#ifndef __LUMASENSOR_H
+#define __LUMASENSOR_H
+
+void Lsensor_Init(void);
+float Lsensor_GetNumber(void);
+#endif
